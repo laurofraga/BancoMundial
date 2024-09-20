578205fabebec5e98f45ffc279ac0dd5bcc0bade
lauro dias <fragalauro12@gmail.com>
2024-09-20 16:42:55 -0300

    feat: add classe Pessoa Juridica

--- models/PessoaFisica.cs
+++ models/PessoaFisica.cs
@@ -1,3 +1,25 @@
 using System;
 
 public class PessoaFisica : Pessoa
+{
+    public string Nome {get; set;}
+    public string Sobrenome {get; set;}
+    public string Rg {get; set;}
+    public string CPF {get; set;}
+    public DateTime DataDeNascimento {get; set;}
+    public int Idade => DateTime.Now.Year - DataDeNascimento.Year;
+    public string FaixaEtaria => Auxiliar.FaixaEtaria(Idade);
+    public double Renda {get; set;}
+
+    public PessoaFisica(string nome, string sobrenome, string rg, string cpf, DateTime dataDeNascimento, string endereco, string telefone, string email, double renda) 
+    : base(endereco, telefone, email)
+    {
+        Nome = nome;
+        Sobrenome = sobrenome;
+        Rg = rg;
+        CPF = cpf;
+        DataDeNascimento = dataDeNascimento;
+        Renda = renda;
+    }
+
+}
